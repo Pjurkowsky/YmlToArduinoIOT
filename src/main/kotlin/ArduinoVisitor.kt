e17bc54e191533acc67d2c4ef23d4153c589e990
xololt-dev <105434979+xololt-dev@users.noreply.github.com>
2025-03-21 10:00:11 +0100

    Throw for mandatory sections, early return for optional (refactor)

--- src/main/kotlin/ArduinoVisitor.kt
+++ src/main/kotlin/ArduinoVisitor.kt
@@ -1,51 +1,60 @@
 import entities.*
 import essa.ExprBaseVisitor
 import essa.ExprParser
-import kotlin.math.sign
 
 class ArduinoVisitor : ExprBaseVisitor<Arduino>() {
     var arduino: Arduino = Arduino()
     var memory: HashMap<String, Any> = HashMap()
 
     override fun visitConfig(ctx: ExprParser.ConfigContext?): Arduino {
-        if (ctx != null) {
-            for (section in ctx.section()) {
-                visit(section)
-            }
+        if (ctx == null) {
+            throw Error("Config section is missing!")
         }
+
+        for (section in ctx.section()) {
+            visit(section)
+        }
+
         return arduino
     }
 
     override fun visitBoardDecl(ctx: ExprParser.BoardDeclContext?): Arduino {
+        if (ctx == null) {
+            throw Error("Board section is missing!")
+        }
+
         var platform: String? = null
         var type: String? = null
         var port: String? = null
-        if (ctx != null) {
-            for (section in ctx.boardSection()) {
-                when {
-                    section.boardPlatform() != null -> platform =
-                        section.boardPlatform().TEXT(0).text + ":" + section.boardPlatform().TEXT(1).text
 
-                    section.boardType() != null -> type = section.boardType().TEXT().text
-                    section.boardPort() != null -> port = section.boardPort().DEVICE_PATH().text
-                }
+        for (section in ctx.boardSection()) {
+            when {
+                section.boardPlatform() != null -> platform =
+                    section.boardPlatform().TEXT(0).text + ":" + section.boardPlatform().TEXT(1).text
+
+                section.boardType() != null -> type = section.boardType().TEXT().text
+                section.boardPort() != null -> port = section.boardPort().DEVICE_PATH().text
             }
-        } else throw Error("Board section not found!")
+        }
         arduino.board = Board(
             platform ?: throw Error("Board platform is missing!"),
             type ?: throw Error("Board type is missing!"),
             port ?: throw Error("Board port is missing!")
         )
+
         return arduino
     }
 
     override fun visitInputsDecl(ctx: ExprParser.InputsDeclContext?): Arduino {
-        if (ctx != null) {
-            for (input in ctx.inputEntry()) {
-                var name: String? = null
-                var mode: String? = null
-                var type: String? = null
-                var source: String? = null
+        if (ctx == null) {
+            throw Error("Inputs section is missing!")
+        }
+
+        for (input in ctx.inputEntry()) {
+            var name: String? = null
+            var mode: String? = null
+            var type: String? = null
+            var source: String? = null
 
             if (input.inputName() != null) {
                 name = when {
@@ -79,39 +88,44 @@ class ArduinoVisitor : ExprBaseVisitor<Arduino>() {
     }
 
     override fun visitOutputsDecl(ctx: ExprParser.OutputsDeclContext?): Arduino {
-        if (ctx != null) {
-            for (output in ctx.outputEntry()) {
-                var name: String? = null
-                var mode: String? = null
-                var pin: String? = null
+        if (ctx == null) {
+            throw Error("Outputs section is missing!")
+        }
 
-                if (output.outputName() != null) {
-                    name = output.outputName().TEXT().text
-                }
-                if (output.outputMode() != null) {
-                    mode = output.outputMode().TEXT().text
-                }
-                if (output.outputPin() != null) {
-                    pin = output.outputPin().TEXT().text
-                }
-                arduino.outputs.add(
-                    Outputs(
-                        name ?: throw Error("Output name is missing!"),
-                        mode ?: throw Error("Output mode is missing!"),
-                        pin ?: throw Error("Output pin is missing!")
-                    )
-                )
+        for (output in ctx.outputEntry()) {
+            var name: String? = null
+            var mode: String? = null
+            var pin: String? = null
+
+            if (output.outputName() != null) {
+                name = output.outputName().TEXT().text
             }
-        } else throw Error("Output section is missing!")
+            if (output.outputMode() != null) {
+                mode = output.outputMode().TEXT().text
+            }
+            if (output.outputPin() != null) {
+                pin = output.outputPin().TEXT().text
+            }
+            arduino.outputs.add(
+                Outputs(
+                    name ?: throw Error("Output name is missing!"),
+                    mode ?: throw Error("Output mode is missing!"),
+                    pin ?: throw Error("Output pin is missing!")
+                )
+            )
+        }
 
         return arduino
     }
 
     override fun visitConstantsDecl(ctx: ExprParser.ConstantsDeclContext?): Arduino {
-        if (ctx != null) {
-            for (constant in ctx.constantEntry()) {
-                var name: String? = null
-                var value: String? = null
+        if (ctx == null) {
+            return arduino
+        }
+
+        for (constant in ctx.constantEntry()) {
+            var name: String? = null
+            var value: String? = null
 
             if (constant.constantName() != null) {
                 name = when {
@@ -143,12 +157,15 @@ class ArduinoVisitor : ExprBaseVisitor<Arduino>() {
     }
 
     override fun visitSignalsDecl(ctx: ExprParser.SignalsDeclContext?): Arduino {
-        if (ctx != null) {
-            for (signal in ctx.signalEntry()) {
-                var name: String? = null
-                var varA: String? = null
-                var varB: String? = null
-                var operand: String? = null
+        if (ctx == null) {
+            return arduino
+        }
+
+        for (signal in ctx.signalEntry()) {
+            var name: String? = null
+            var varA: String? = null
+            var varB: String? = null
+            var operand: String? = null
 
             if (signal.singalName() != null) {
                 name = when {
@@ -171,65 +188,71 @@ class ArduinoVisitor : ExprBaseVisitor<Arduino>() {
                     operand ?: throw Error("Signal operand is missing!")
                 )
             )
-            memory[name] = false;
+            memory[name] = false
         }
+
         return arduino
     }
 
     override fun visitRulesDecl(ctx: ExprParser.RulesDeclContext?): Arduino {
-        if (ctx != null) {
-            var ruleVariable: String? = null
-            var ruleDo: String? = null
-            var ruleVar: String? = null
-            var ruleNot: String? = null
-            var ruleState: String? = null
-            for (rule in ctx.ruleEntry()) {
-                if (rule.ruleIf() != null) {
-                    ruleVariable = rule.ruleIf().variable.text
-                    if (rule.ruleIf().donot != null)
-                        ruleNot = rule.ruleIf().donot.text
-                }
-                if (rule.ruleThen() != null) {
-                    ruleDo = rule.ruleThen().do_.text
-                    ruleVar = rule.ruleThen().variable.text
-                    ruleState = rule.ruleThen().state.text
-                }
-                arduino.rules.add(
-                    Rules(
-                        ruleVariable ?: throw Error("Rule if is missing!"),
-                        ruleDo ?: throw Error("Rule do is missing!"),
-                        ruleNot,
-                        ruleVar ?: throw Error("Rule var is missing!"),
-                        ruleState ?: throw Error("Rule state is missing!")
-                    )
-                )
-            }
+        if (ctx == null) {
+            return arduino
         }
+
+        var ruleVariable: String? = null
+        var ruleDo: String? = null
+        var ruleVar: String? = null
+        var ruleNot: String? = null
+        var ruleState: String? = null
+        for (rule in ctx.ruleEntry()) {
+            if (rule.ruleIf() != null) {
+                ruleVariable = rule.ruleIf().variable.text
+                if (rule.ruleIf().donot != null)
+                    ruleNot = rule.ruleIf().donot.text
+            }
+            if (rule.ruleThen() != null) {
+                ruleDo = rule.ruleThen().do_.text
+                ruleVar = rule.ruleThen().variable.text
+                ruleState = rule.ruleThen().state.text
+            }
+
+            arduino.rules.add(
+                Rules(
+                    ruleVariable ?: throw Error("Rule if is missing!"),
+                    ruleDo ?: throw Error("Rule do is missing!"),
+                    ruleNot,
+                    ruleVar ?: throw Error("Rule var is missing!"),
+                    ruleState ?: throw Error("Rule state is missing!")
+                )
+            )
+        }
+
         return arduino
     }
 
     override fun visitEventsDecl(ctx: ExprParser.EventsDeclContext?): Arduino {
-        if (ctx != null) {
-            var eventWhen: String? = null
-            var eventDo: String? = null
-            for (event in ctx.eventEntry()) {
-                if (event.eventWhen() != null) {
-                    eventWhen = event.eventWhen().TEXT().text
-                }
-                if (event.eventDo() != null) {
-                    eventDo = event.eventDo().TEXT().text
-                }
-                arduino.events.add(
-                    Events(
-                        eventWhen ?: throw Error("Event when is missing!"),
-                        eventDo ?: throw Error("Event do is missing!")
-                    )
-                )
-            }
+        if (ctx == null) {
+            return arduino
         }
+
+        var eventWhen: String? = null
+        var eventDo: String? = null
+        for (event in ctx.eventEntry()) {
+            if (event.eventWhen() != null) {
+                eventWhen = event.eventWhen().TEXT().text
+            }
+            if (event.eventDo() != null) {
+                eventDo = event.eventDo().TEXT().text
+            }
+            arduino.events.add(
+                Events(
+                    eventWhen ?: throw Error("Event when is missing!"),
+                    eventDo ?: throw Error("Event do is missing!")
+                )
+            )
+        }
+
         return arduino
     }
-
-
 }
 
